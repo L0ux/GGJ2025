fe82e4bf05a41d9b99e250829644904b88f8db42
BourgonNils <43369094+BourgonNils@users.noreply.github.com>
2025-01-25 12:23:12 +0100

    ça charge des sauts

--- bubble-game/Assets/Scripts/MoveInTheAir.cs
+++ bubble-game/Assets/Scripts/MoveInTheAir.cs
@@ -2,15 +2,30 @@ using UnityEngine.InputSystem;
 using UnityEngine;
 using Unity.VisualScripting;
 using static UnityEditor.Searcher.SearcherWindow.Alignment;
+using UnityEngine.Rendering;
 
 public class MoveInTheAir : MonoBehaviour
 {
     [SerializeField] float customGravity = 2; // Accélération gravité personnalisée
-    [SerializeField] float horizontalMovementSpeed = 50f;
-    Vector2 movementDirection;
+    [SerializeField] float horizontalMovementSpeed = 5f;
+
+    [SerializeField] float maxJumpForce = 30;
+
+    [SerializeField] float maxChargeTimeJump;
+    [SerializeField] float decelarationHorizontal;
+    [SerializeField] Vector2 TMP_JUMP_DIRECTION;
+
+    float TMP_jump_force = 30;
+
     Rigidbody2D rb;
     float horizontalMoveDirection;
     bool accrochedAuSavon;
+
+
+    float _timeChargingJump = 0f;
+    
+
+
 
 
     void Start()
@@ -18,41 +33,90 @@ public class MoveInTheAir : MonoBehaviour
         rb = this.GetComponent<Rigidbody2D>();
         stick();
 
-
     }
    
     // Update is called once per frame
     void Update()
     {
+        /***************MOUVEMENT HORIZONTAL************/
+
+        /*On lit l'input*/
         horizontalMoveDirection = Input.GetAxis("Horizontal");
 
-        rb.linearVelocityX = horizontalMoveDirection * horizontalMovementSpeed * Time.fixedDeltaTime;
-        
-        Debug.Log("Test " + movementDirection.x * horizontalMovementSpeed);
+        rb.AddForceX(horizontalMoveDirection * horizontalMovementSpeed);
+
+        /*Freinage*/
+        /* ?? peut être mettre le freinage uniquement si on n'appuie pas sur la touche ?? */
+        rb.linearVelocityX = Mathf.MoveTowards(rb.linearVelocityX, 0, decelarationHorizontal * Time.fixedDeltaTime);
+
+        /*?? Ajouter une vitesse max ??*/
+
+
+        /***************GESTION DU SAUT************/
+
+        if(Input.GetKey(KeyCode.Space) && accrochedAuSavon)
+        {
+            _timeChargingJump += Time.deltaTime;
+            Debug.Log("On charge le saut charge Time : " + _timeChargingJump);
+        }
+        else 
+        {
+            if (_timeChargingJump > 0 && accrochedAuSavon)
+            {
+                _timeChargingJump = Mathf.Max(_timeChargingJump, maxChargeTimeJump);
+                
+                /*On récup le pourcentage de temps appuyé,
+                 * 0 si 0secondes 
+                 * 1 si appuyé maxChargeTimeJump*/
+                float jumpPercent = Mathf.Lerp(0, maxChargeTimeJump, _timeChargingJump);
+                doAJump(TMP_JUMP_DIRECTION, jumpPercent * maxJumpForce);
+                
+            }
+            _timeChargingJump = 0;
+        }
+
+
     }
 
     void FixedUpdate()
     {
         if(!accrochedAuSavon)
             rb.linearVelocityY += customGravity * Time.fixedDeltaTime;
+
     }
 
-    public void OnJump()
+
+    void doAJump(Vector2 jumpDirection, float jumpForce)
     {
         unstick();
+        rb.AddForce(jumpDirection * jumpForce, ForceMode2D.Impulse);
+        Debug.Log("On saute avec une force de " + jumpForce);
     }
+
+
+
+
+
+
+    /*Appelé par la barre d'espace*//*
+    public void OnJump()
+    {
+        if (!accrochedAuSavon)
+            return;
+        
+        
+        doAJump(TMP_JUMP_DIRECTION);
+    }*/
 
 
     void stick()
     {
         accrochedAuSavon = true;
-
     }
 
 
     void unstick()
     {
         accrochedAuSavon = false;
-
     }
 }
