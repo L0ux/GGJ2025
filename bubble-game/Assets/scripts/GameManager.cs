a0382f2654edeeb7697c58934d229c0200f3439c
L0ux <guillaumeloux@gmail.com>
2025-01-25 12:10:05 +0100

    multiple thing

---
+++ bubble-game/Assets/scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance { get; private set; }
+
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    public void RestartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Jeu quitté !");
+    }
+}
