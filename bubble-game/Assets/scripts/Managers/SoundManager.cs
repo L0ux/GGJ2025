0970593417c07f73c529f25d70d68fa19d3b0347
L0ux <guillaumeloux@gmail.com>
2025-01-26 15:45:03 +0100

    updateBuild

--- bubble-game/Assets/scripts/Managers/SoundManager.cs
+++ bubble-game/Assets/scripts/Managers/SoundManager.cs
@@ -1,14 +1,10 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class SoundManager : MonoBehaviour
 {
     public AudioSource musicSource;    // Référence à l'AudioSource pour la musique
     public AudioClip musicClip;        // Clip de musique à jouer en boucle
     private static SoundManager instance; // Instance statique pour gérer le singleton
-
-
-    public SceneManager sceneManager;
 
     void Awake()
     {
@@ -20,10 +16,6 @@ public class SoundManager : MonoBehaviour
         }
         else
         {
-            if (sceneManager.IsLastScene())
-            {
-                PlayMusicForLastScene();
-            }
             instance = this;
             DontDestroyOnLoad(gameObject);  // Cela permet de garder l'objet lors du changement de scène
         }
@@ -56,13 +48,5 @@ public class SoundManager : MonoBehaviour
         {
             musicSource.Stop();
         }
-    }
-
-    private void PlayMusicForLastScene()
-    {
-        StopMusic();
-        musicSource.clip = musicClip;
-        musicSource.loop = false; 
-        musicSource.Play();
     }
 }
